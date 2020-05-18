6d7694b80ad9feff1e9efd92eb540c21247eee6c
feddersmichael <fedders.michael@gmail.com>
2020-05-19 01:17:53 +0200

    numerical integration

--- numerical_testing.py
+++ numerical_testing.py
@@ -1,21 +1,7 @@
 import scipy.integrate as integrate
 import math
 import numpy as np
-
-
-def integrand(c, s, y, h, N):
-    """integrand of our marginalisation
-
-    c and s = sigma^2 are the integration variables.
-    y and h are represnting the measured data at time points,
-    so they com in lists of length N, which is the amount
-    of time-points."""
-    prefactor = 1 / ((2 * math.pi * s) ** (N / 2))
-    integr = 0
-    for k in range(N):
-        integr += (y[k] - c - h[k]) ** 2
-    integr = math.expm1(integr / (2 * s))
-    return prefactor * integr
+import scipy
 
 
 def h_func(t, theta):
@@ -38,9 +24,30 @@ def y_measured(t, c, theta, sigma):
     return np.random.normal(y_orig(c, t, theta), sigma)
 
 
+def integrand(c, s, y, h, N):
+    """integrand of our marginalisation
+
+    c and s = sigma^2 are the integration variables.
+    y and h are represnting the measured data at time points,
+    so they com in lists of length N, which is the amount
+    of time-points."""
+    prefactor = 1 / ((2 * math.pi * s) ** (N / 2))
+    integr = 0
+    for k in range(N):
+        integr += (y[k] - c - h[k]) ** 2
+    integr = math.expm1(integr / (2 * s))
+    return prefactor * integr
+
+
 def main():
     c = 0
-    h = [h_func(n, 0) for n in range(1, 10)]
-    y = [y_measured(n, c, 0, 1) for n in range(1, 10)]
+    N = 10
+    theta = 0
+    sigma = 1
+    h = [h_func(n, theta) for n in range(1, N+1)]
+    y = [y_measured(n, c, theta, sigma) for n in range(1, N+1)]
+    result = scipy.integrate.dblquad(integrand, 0.1, 3, lambda x: -3, lambda y: 3, args=(y, h, N))
 
+
+main()
 # ans, err = scipy.integrate.dblquad(integrand, 0, 20, lambda x: -10, lambda x: 10)
