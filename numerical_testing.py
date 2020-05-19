93d1b84e61b7840b8308d0bdc768300ef40fc4d6
feddersmichael <fedders.michael@gmail.com>
2020-05-19 18:59:09 +0200

    implementation analytical solution

--- numerical_testing.py
+++ numerical_testing.py
@@ -1,6 +1,7 @@
 import scipy.integrate as integrate
 import math
 import numpy as np
+import scipy.special as special
 import scipy
 
 
@@ -39,15 +40,28 @@ def integrand(c, s, y, h, N):
     return prefactor * integr
 
 
+def analytical(y, h, N):
+    product_1 = N ** (N / 2 - 2)
+    product_2 = 2 * np.pi ** ((N - 1) / 2)
+    sum_1 = np.sum((h - y) ** 2)
+    sum_2 = np.sum(y - h) ** 2
+    product_3 = (sum_1 - sum_2) ** (-(N - 3) / 2)
+    return product_1 * product_3 * special.gamma((N - 3) / 2) / product_2
+
+
 def main():
     c = 0
     N = 10
     theta = 0
     sigma = 1
-    h = [h_func(n, theta) for n in range(1, N+1)]
-    y = [y_measured(n, c, theta, sigma) for n in range(1, N+1)]
+    h = np.zeros(N)
+    y = np.zeros(N)
+    for n in range(N):
+        h[n] = h_func(n + 1, theta)
+        y[n] = y_measured(n + 1, c, theta, sigma)
+    solution = analytical(y, h, N)
     result = scipy.integrate.dblquad(integrand, 0.1, 3, lambda x: -3, lambda y: 3, args=(y, h, N))
+    print(result)
 
 
 main()
-# ans, err = scipy.integrate.dblquad(integrand, 0, 20, lambda x: -10, lambda x: 10)
