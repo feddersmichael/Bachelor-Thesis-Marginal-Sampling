14285bb2d065a3809a1918cdad926da48724a9cf
feddersmichael <fedders.michael@gmail.com>
2020-12-03 15:26:45 +0100

    plots

--- pipeline_for_sampling/datamerge_CR.py
+++ pipeline_for_sampling/datamerge_CR.py
@@ -8,6 +8,7 @@ import numpy as np
 from scipy.special import gammaln
 import pickle
 import os
+import matplotlib.pyplot as plt
 
 petab_problem = petab.Problem.from_yaml(
     "conversion_reaction/SS_conversion_reaction.yaml")
@@ -141,12 +142,10 @@ def merge_full_parameter():
     data_full_sampling = [0] * 50
     length = 0
 
-
     for n in range(50):
         with open(d + '\\Results_FP_CR\\result_FP_CR_' + str(n) + '.pickle', 'rb') as infile_1:
             data_full_sampling[n] = pickle.load(infile_1)  # pickle.load(infile_1)
             length += 10001 - data_full_sampling[n].burn_in
-    length += data_full_sampling[0].burn_in
 
     merged_data = pypesto.Result(standard_sampling())
 
@@ -161,14 +160,35 @@ def merge_full_parameter():
 
     merged_data.sample_result.betas = np.array([1])
 
-    merged_data.sample_result.burn_in = data_full_sampling[0].burn_in
+    merged_data.sample_result.burn_in = 0
 
     merged_data.sample_result.auto_correlation = None
     sample.effective_sample_size(merged_data)
+
+    index = 0
     for n in range(50):
-        pass
+        burn_in = data_full_sampling[n].burn_in
+        converge_size = 10001 - burn_in
+        merged_data.sample_result.trace_x[0, index:index + converge_size, :] \
+            = data_full_sampling[n].trace_x[0, burn_in:, :]
+        merged_data.sample_result.trace_neglogpost[0, index:index + converge_size] \
+            = data_full_sampling[n].trace_neglogpost[0, burn_in:]
+        merged_data.sample_result.trace_neglogprior[0, index:index + converge_size] \
+            = data_full_sampling[n].trace_neglogprior[0, burn_in:]
+        index += converge_size
+    with open(d + '\\Results_FP_CR\\merged_data_FP_CR.pickle', 'wb') as save_file:
+        pickle.dump(merged_data, save_file)
 
+    fig = plt.figure(figsize=(12, 5))
+    ax0 = fig.add_subplot(1, 3, 1)
+    ax1 = fig.add_subplot(1, 3, 2)
+    ax2 = fig.add_subplot(1, 3, 3)
 
+    ax0 = visualize.sampling_fval_trace(merged_data, size=(12, 5), full_trace=True)
+    ax1 = visualize.sampling_parameters_trace(merged_data, use_problem_bounds=False, full_trace=True, size=(12, 5))
+    ax2 = visualize.sampling_1d_marginals(merged_data, size=(12, 5))
+
+    plt.show()
 
 
 def merge_marginalised_parameter():
@@ -180,6 +200,8 @@ def merge_marginalised_parameter():
 
 def main():
     merge_full_parameter()
+    n = 1
+    return 0
 
 
 main()
